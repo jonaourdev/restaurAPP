0006534de1806c55c3949e03a27f264c92b518ec
Lucas <lu.urzua@duocuc.cl>
2025-10-17 00:48:11 -0300

    Implementacion logica de registro
    Se implementó:
    ViewModelProvider uso de Factory
    Lógica de registro
    UI de registro
    
    **FALTA COMPROBAR FUNCIONAMIENTO**

--- app/src/main/java/com/example/restaurapp/MainActivity.kt
+++ app/src/main/java/com/example/restaurapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -16,9 +17,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.restaurapp.ui.theme.RestaurAppTheme
 import com.example.restaurapp.ui.HomeScreen
+import com.example.restaurapp.viewmodel.RegisterViewModel
+import com.example.restaurapp.viewmodel.RegisterViewModelFactory
 
 
 class MainActivity : ComponentActivity() {
+
+    private val vmRegister: RegisterViewModel by viewModels {
+        RegisterViewModelFactory(application)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
