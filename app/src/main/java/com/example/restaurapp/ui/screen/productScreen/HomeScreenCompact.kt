dff34b3983df8f6a968eebcb54a12cf240a66fad
MarcoCO99 <marc.contrerasa@duocuc.cl>
2025-10-20 02:51:33 -0300

    rename Screens Home

--- app/src/main/java/com/example/restaurapp/ui/screen/productScreen/HomeScreenCompact.kt
+++ app/src/main/java/com/example/restaurapp/ui/screen/productScreen/HomeScreenCompact.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
