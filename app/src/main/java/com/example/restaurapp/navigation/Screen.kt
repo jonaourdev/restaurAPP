3db6b94d1aa7bee6acacd5198ec89566a9546d51
MarcoCO99 <marc.contrerasa@duocuc.cl>
2025-10-20 05:06:31 -0300

    add formative and technical concepts screens to navigation

--- app/src/main/java/com/example/restaurapp/navigation/Screen.kt
+++ app/src/main/java/com/example/restaurapp/navigation/Screen.kt
@@ -7,4 +7,18 @@ sealed class Screen(val route: String) {
     data object Profile : Screen("profile")
     data object Favorites : Screen("favorites")
     data object EditProfile : Screen("edit_profile")
+
+    data object TechnicalConcept : Screen("technical_concept")
+
+    data object FormativeConcept : Screen("formative_concept")
+
+    data object AddContent : Screen("add_content")
+
+    data class FormativeDetail(val conceptId: String) : Screen("formative_detail/$conceptId") {
+        companion object {
+            const val Route_base = "formative_detail"
+            const val Arg_concept_id = "conceptId"
+            const val full_route = "$Route_base/{$Arg_concept_id}"
+        }
+    }
 }
